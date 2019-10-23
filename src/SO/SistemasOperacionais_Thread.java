9abd67b48076bcc0580d0edd62de4dcdf18bd054
aluno <aluno@l7m05-linux.campusrp-lab.riopomba.ifsudestemg.edu.br>
2019-10-23 11:08:38 -0300

    Vet added

--- src/SO/SistemasOperacionais_Thread.java
+++ src/SO/SistemasOperacionais_Thread.java
@@ -6,68 +6,48 @@ public class SistemasOperacionais_Thread extends Somador {
 
     public static void main(String[] args) throws InterruptedException {
 
-//        Random gerador = new Random();
-//
-//        Somador somador1 = new Somador();
-//        somador1.setPriority(MIN_PRIORITY);
-//
-//        Somador somador2 = new Somador();
-//        somador2.setPriority(NORM_PRIORITY);
-//
-//        Somador somador3 = new Somador();
-//        somador3.setPriority(MAX_PRIORITY);
-//
-//        Somador somador4 = new Somador();
-//        somador4.setPriority(MAX_PRIORITY);
-//
-//        for (int i = 0; i < 100; i++) {
-//            somador1.vetor[i] = gerador.nextInt(9);
-//            somador2.vetor[i] = gerador.nextInt(9);
-//            somador3.vetor[i] = gerador.nextInt(9);
-//            somador4.vetor[i] = gerador.nextInt(9);
-//
-//        }
-//
-//        somador1.start();
-//        somador2.start();
-//        somador3.start();
-//        somador4.start();
-        //cria três tarefas
-        Somador t1 = new Somador(0, 25000);
+        Random gerador = new Random();
+        int vet[] = new int[100000];
+        for (int i = 0; i < 10; i++) {
+            vet[i] = gerador.nextInt(9);
+        }
+
+        Somador t1 = new Somador();
         t1.setName("Tarefa1");
-        
         t1.setPriority(MIN_PRIORITY);
-        Somador t2 = new Somador(25001, 50000);
+        t1.v = vet;
+
+        Somador t2 = new Somador();
         t2.setName("Tarefa2");
         t2.setPriority(MAX_PRIORITY);
-        
-        Somador t3 = new Somador(50001, 75000);
+        t2.v = vet;
+
+        Somador t3 = new Somador();
         t3.setName("Tarefa3");
-        
         t3.setPriority(MAX_PRIORITY);
-        Somador t4 = new Somador(75001, 100000);
+        t3.v = vet;
+
+        Somador t4 = new Somador();
         t4.setName("Tarefa4");
         t4.setPriority(NORM_PRIORITY);
+        t4.v = vet;
 
-        //inicia a execução paralela das três tarefas, iniciando três novas threads no programa
         t1.start();
         t2.start();
         t3.start();
         t4.start();
-
 
         t1.join();
         t2.join();
         t3.join();
         t4.join();
 
-        //Exibimos o somatório dos totalizadores de cada Thread
         System.out.println("Total: " + (t1.getTotal() + t2.getTotal() + t3.getTotal() + t4.getTotal()));
 
     }
 
-    public SistemasOperacionais_Thread(int valorInicial, int valorFinal) {
-        super(valorInicial, valorFinal);
+    public SistemasOperacionais_Thread() {
+        super();
     }
 
 }
--- src/SO/Somador.java
+++ src/SO/Somador.java
@@ -1,34 +1,24 @@
 package SO;
 
 public class Somador extends Thread {
-
-    private final long valorInicial;
-    private final long valorFinal;
     private long total = 0;
+    int v[];
 
-    //método construtor que receberá os parâmetros da tarefa
-    public Somador(int valorInicial, int valorFinal) {
-        this.valorInicial = valorInicial;
-        this.valorFinal = valorFinal;
+    public Somador() {
+        this.v = new int[100000];
     }
 
-    //método que retorna o total calculado
     public long getTotal() {
         return total;
     }
 
-    /*
-     Este método se faz necessário para que possamos dar start() na Thread 
-     e iniciar a tarefa em paralelo
-     */
-    @Override
     public void run() {
 
         System.out.println("\nNome: " + Thread.currentThread().getName());
-        //System.out.println("Prioridade: " + Thread.currentThread().getPriority());
+        System.out.println("Prioridade: " + Thread.currentThread().getPriority());
 
-        for (long i = valorInicial; i <= valorFinal; i++) {
-            total += i;
+        for (int i = 0; i < 100000; i++) {
+            total += v[i];
         }
     }
 }
